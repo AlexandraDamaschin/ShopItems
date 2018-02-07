434ea92cad16ffd5696389d2ffa6d71877ab9075
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-07 12:22:56 +0000

    SHOP ITEM REPOSITORY INTERFACE

---
+++ ShopItems/DAL/IShopItemRepository.cs
@@ -0,0 +1,21 @@
+﻿using ShopItems.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopItems.DAL
+{
+    public interface IShopItemRepository: IDisposable
+    {
+        IEnumerable<ShopItem> GetItems();
+        ShopItem GetItemById(int userID);
+
+        void InsertItem(ShopItem Item);
+        void UpdateItem(ShopItem Item);
+        void DeleteItem(ShopItem Item);
+
+        void Save();
+    }
+}
