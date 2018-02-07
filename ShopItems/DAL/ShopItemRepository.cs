07ed67f8e11ad332ffa1dbffdbd216142441f63a
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-07 12:31:40 +0000

    Shop Item Repo

---
+++ ShopItems/DAL/ShopItemRepository.cs
@@ -0,0 +1,53 @@
+﻿using ShopItems.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopItems.DAL
+{
+    public class ShopItemRepository :IShopItemRepository, IDisposable
+    {
+        ShopItemsContext context;
+        
+        public ShopItemRepository(ShopItemsContext context)
+        {
+            this.context = context;
+        }
+
+        public void DeleteItem (int ItemID)
+        {
+
+        }
+
+        public ShopItem GetItemByID(int itemID)
+        {
+
+        }
+
+        public  IEnumerable<ShopItem> GetItems()
+        {
+
+        }
+
+        public void InsertItem (ShopItem item)
+        {
+
+        }
+
+        public void Save()
+        {
+          
+        }
+
+        public void UpdateItem (ShopItem item)
+        {
+
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
