13ad0a7e2bde519cd883412d4728f0150d0beb7d
Alexandra Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-07 12:19:42 +0000

    USER REPOSITORY INTERFACE

---
+++ ShopItems/DAL/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using ShopItems.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopItems.DAL
+{
+     public interface IUserRepository : IDisposable
+    {
+        IEnumerable<User> GetUsers();
+        User GetUserByID(int userID);
+
+        void InsertUser(User user);
+        void UpdateUser(User user);
+        void DeleteUser(int userID);
+        void Save();
+    }
+}
