ee54a12941a9af5c137543a68fcaa2e96d32c0ef
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-13 18:49:37 +0000

    items and users???

--- ShopItems/DAL/ShopItemRepository.cs
+++ ShopItems/DAL/ShopItemRepository.cs
@@ -2,44 +2,52 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ShopItems.DAL
 {
     public class ShopItemRepository :IShopItemRepository, IDisposable
     {
         ShopItemsContext context;
-        
+
         public ShopItemRepository(ShopItemsContext context)
         {
             this.context = context;
         }
 
+        //•	DeleteItem should delete the user with the matching ID
         public void DeleteItem (int ItemID)
         {
-
+            
         }
 
+        //•	GetItemByID should get the specified user with the matching ID
         public ShopItem GetItemByID(int itemID)
         {
-
+           
         }
 
-        public  IEnumerable<ShopItem> GetItems()
+        //•	GetItems should return all Users
+        public IEnumerable<ShopItem> GetItems()
         {
 
         }
 
+        //•	InsertItem should insert a new user
         public void InsertItem (ShopItem item)
         {
 
         }
 
+        //•	Save should call context.SaveChanges
         public void Save()
         {
-          
+            context.SaveChanges();
         }
 
+        //•	UpdateItem should set the state of the selected user to Modified
         public void UpdateItem (ShopItem item)
         {
 
