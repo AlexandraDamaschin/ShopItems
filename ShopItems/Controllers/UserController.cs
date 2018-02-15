55bb42e79d049a35bc51b67562ae169def5eb36f
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-15 15:34:53 +0000

    new controller

---
+++ ShopItems/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using ShopItems.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopItems.Controllers
+{
+    public class UserController : Controller
+    {
+        private IUserRepository userRepo;
+        public UserController(IUserRepository repo)
+        {
+            userRepo = repo;
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View(userRepo.GetUsers());
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(userRepo.GetUserByID(id));
+        }
+
+    }
+}
