7059aec0dbc0de126281e0b5795891c76decb082
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-02-17 11:43:31 +0000

    shop items working

--- ShopItems/Controllers/ShopItemsController.cs
+++ ShopItems/Controllers/ShopItemsController.cs
@@ -2,7 +2,9 @@
 using ShopItems.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -10,23 +12,123 @@ namespace ShopItems.Controllers
 {
     public class ShopItemsController : Controller
     {
-        private IShopItemRepository repo;
+        private ShopItemsContext db = new ShopItemsContext();
 
-        public ShopItemsController(IShopItemRepository shopRepo)
+        private IShopItemRepository shopRepo;
+
+        public ShopItemsController()
         {
-            repo = shopRepo;
+            shopRepo = new DAL.ShopItemRepository(new ShopItemsContext());
         }
+
         // GET: ShopItems
         public ActionResult Index()
         {
-            return View(repo.GetItems());
+            return View(db.ShopItems.ToList());
         }
 
         // GET: ShopItems/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int? id)
         {
-            return View(repo.GetItemByID(id));
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShopItem shopItem = db.ShopItems.Find(id);
+            if (shopItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shopItem);
         }
 
+        // GET: ShopItems/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ShopItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Name,Cost,Class,ImageURL")] ShopItem shopItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ShopItems.Add(shopItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shopItem);
+        }
+
+        // GET: ShopItems/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShopItem shopItem = db.ShopItems.Find(id);
+            if (shopItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shopItem);
+        }
+
+        // POST: ShopItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Cost,Class,ImageURL")] ShopItem shopItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shopItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shopItem);
+        }
+
+        // GET: ShopItems/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ShopItem shopItem = db.ShopItems.Find(id);
+            if (shopItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shopItem);
+        }
+
+        // POST: ShopItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ShopItem shopItem = db.ShopItems.Find(id);
+            db.ShopItems.Remove(shopItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
 }
